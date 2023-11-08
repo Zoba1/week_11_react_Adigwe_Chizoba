eca4d15a42c6eaef5804451a173e41083ba26aba
Kenpachi2 <ifeanyiumeadi201@gmail.com>
2023-11-08 05:50:28 -0800

    add route

---
+++ src/Component/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form action="">
+        <input type="text" placeholder='name' />
+        <textarea name="comm-area" id="comment" cols="30" rows="10"></textarea>
+    </form>
+    )
+}
+
+export default Form
---
+++ src/Component/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../'
+
+function Header() {
+  return (
+    <div>
+        <div>
+            <h1>Zobbs Caterrry!!!</h1>
+            <nav>
+                <button>our pantry</button>
+                <button>About us</button>
+                <button>Contact us</button>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
